775df9e7e044e808637a0a7c1087d86fd7435b26
xudong <xudong.he@grabtaxi.com>
2017-10-11 15:30:49 +0800

    store data in localstorage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import persistState from 'redux-localstorage'
 import reducer from './reducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -8,7 +9,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
     composeEnhancers(
-        applyMiddleware(ReduxThunk)
+        applyMiddleware(ReduxThunk),
+        persistState()
     )
 );
 export default store;
